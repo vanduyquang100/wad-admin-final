5b150d0e4d097f354d0fdec4dab73020f9da1f80
vanduyquang100 <vanduyquang2020@gmail.com>
2024-12-22 15:38:27 +0700

    Allow sorting in user list

--- src/components/pages/UserListPage.tsx
+++ src/components/pages/UserListPage.tsx
@@ -1,6 +1,7 @@
 import {
   Badge,
   BasicLayout,
+  Button,
   Pagination,
   PaginationContent,
   PaginationItem,
@@ -15,21 +16,49 @@ import {
 } from "@/components/ui";
 import { useUsers } from "@/hooks";
 import LoadingPage from "./LoadingPage";
-import { useSearchParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+import { useCallback, useEffect, useState } from "react";
+import { NAVIGATION_ROUTES } from "@/constants/apis";
 
 const PAGE_LIMIT = 10;
 
 export const UserListPage = () => {
   const [params] = useSearchParams();
   const [page, setPage] = useState(parseInt(params.get("page") ?? "1"));
+  const [sortBy, setSortBy] = useState<[string, boolean] | undefined>(
+    undefined
+  );
+
+  const setSortCallback = useCallback(
+    (sortColumn: string) => () => {
+      setSortBy((prev) => {
+        const isSameColumn = (prev ? prev[0] : undefined) === sortColumn;
+        if (isSameColumn) {
+          return [sortColumn, prev ? !prev[1] : true];
+        }
+        return [sortColumn, true];
+      });
+    },
+    []
+  );
+
+  const setSortEmail = useCallback(setSortCallback("email"), [setSortCallback]);
+  const setSortName = useCallback(setSortCallback("name"), [setSortCallback]);
+  const setSortCreatedDate = useCallback(setSortCallback("createdAt"), [
+    setSortCallback,
+  ]);
 
   useEffect(() => {
     setPage(parseInt(params.get("page") ?? "1"));
   }, [params]);
 
   const { data, isFetching } = useUsers({
-    filters: { page: page, limit: PAGE_LIMIT },
+    filters: {
+      page: page,
+      limit: PAGE_LIMIT,
+      sortBy: sortBy ? sortBy[0] : undefined,
+      sortOrder: sortBy && sortBy[1] ? "asc" : "desc",
+    },
   });
 
   if (isFetching) {
@@ -42,10 +71,34 @@ export const UserListPage = () => {
         <TableHeader>
           <TableRow>
             <TableHead className="w-[100px]">No.</TableHead>
-            <TableHead>Email</TableHead>
-            <TableHead>Name</TableHead>
-            <TableHead>Roles</TableHead>
-            <TableHead>Created Date</TableHead>
+            <TableHead>
+              <Button
+                variant="ghost"
+                className="w-full justify-start"
+                onClick={setSortEmail}
+              >
+                Email
+              </Button>
+            </TableHead>
+            <TableHead>
+              <Button
+                variant="ghost"
+                className="w-full justify-start"
+                onClick={setSortName}
+              >
+                Name
+              </Button>
+            </TableHead>
+            <TableHead align="center">Roles</TableHead>
+            <TableHead>
+              <Button
+                variant="ghost"
+                className="w-full justify-start"
+                onClick={setSortCreatedDate}
+              >
+                Created Date
+              </Button>
+            </TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
@@ -55,9 +108,15 @@ export const UserListPage = () => {
                 <TableCell className="font-medium">
                   {index + (page - 1) * PAGE_LIMIT + 1}
                 </TableCell>
-                <TableCell>{user.email}</TableCell>
+                <TableCell>
+                  <Link
+                    to={NAVIGATION_ROUTES.USER_DETAIL.replace(":id", user._id)}
+                  >
+                    {user.email}
+                  </Link>
+                </TableCell>
                 <TableCell>{user.name}</TableCell>
-                <TableCell className="flex space-x-2">
+                <TableCell>
                   {user.roles.map((role) => (
                     <Badge key={role} variant="secondary">
                       {role}
